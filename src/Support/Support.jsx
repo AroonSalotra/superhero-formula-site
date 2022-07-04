9912b9e2a32deb1daf26765938b12bfb4cb28c8a
Aroon Salotra <aroonsalotra@gmail.com>
2022-07-05 00:17:41 +0100

    add survey

--- src/Support/Support.jsx
+++ src/Support/Support.jsx
@@ -3,10 +3,11 @@ import SupportBlock from "./SupportBlock";
 
 const Support = () => {
     return (<div>
+            <h1>Support Page</h1>
         <div className="supportContainer">
-            <SupportBlock title="Product" description="Contact (from 9am to 6pm)" link="[Phone Number]" />
-            <SupportBlock title="Account" description="for accounts" link="[Phone Number]" />
-            <SupportBlock title="Query" description="for queries" link="[Phone Number]" />
+            <SupportBlock title="Product" description="Contact [9am to 6pm]" link="[Phone Number]" />
+            <SupportBlock title="Account" description="Contact [24/7]" link="[Phone Number]" />
+            <SupportBlock title="Query" description="Contact [24/7]" link="[Phone Number]" />
         </div>
         <SupportForm title="Alternatively, Contact Us Directly" />
     </div>);
--- src/Support/SupportForm.jsx
+++ src/Support/SupportForm.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react"
+
 const SupportForm = (props) => {
+
+    const [display, setDisplay] = useState("none")
+
     return (
         <div>
             <h3>
                 {props.title}
             </h3>
             <form action="" className="supportForm">
-                <select name="" id="supportSelect">
-                    <option value="" disabled>Type of Problem</option>
-                    <option value="">Account</option>
-                    <option value="">Product</option>
-                    <option value="">Query</option>
+                <select name="" id="supportSelect" onChange={() => setDisplay("block")} defaultValue={"default"}>
+                    <option value="default" disabled>Type of Problem</option>
+                    <option value="1">Account</option>
+                    <option value="2">Product</option>
+                    <option value="3">Query</option>
                 </select>
-                <textarea name="" id="supportText" cols="30" rows="10" placeholder="Describe your Issue" required/>
-                <input type="email" name="email" id="supportEmail" placeholder="Email" required />
-                <input type="file" name="file" id="supportFile" />
-                <button type="submit" id="supportSubmit">Submit</button>
+                <textarea style={{ display: display }} name="" id="supportText" cols="30" rows="10" placeholder="Describe your Issue" required />
+                <input style={{ display: display }} type="email" name="email" id="supportEmail" placeholder="Email" required />
+                <input style={{ display: display }} type="file" name="file" id="supportFile" />
+                <button style={{ display: display }} type="submit" id="supportSubmit">Submit</button>
             </form>
         </div>
     )
